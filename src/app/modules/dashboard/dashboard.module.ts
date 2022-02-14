565de0c765d4c5340cc161fc1786273a394bf6be
DarwinMMC <darwin.cerpa1@gmail.com>
2022-02-14 11:47:39 -0500

    config gridster2

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingRoutingModule } from './dashboard-routing.module';
+import { GridsterModule } from 'angular-gridster2';
 
 
 
@@ -11,7 +12,8 @@ import { DashboardRoutingRoutingModule } from './dashboard-routing.module';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingRoutingModule
+    DashboardRoutingRoutingModule,
+    GridsterModule
   ]
 })
 export class DashboardModule { }
--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GridsterConfig, GridsterItem } from 'angular-gridster2';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  /** Options config gridster2 */
+  options: GridsterConfig = {
+    gridType: 'verticalFixed',
+    displayGrid: 'onDrag&Resize',
+    pushItems: true,
+    draggable: {
+      enabled: true,
+    },
+    resizable: {
+      enabled: true,
+    },
+    margin: 16,
+    minCols: 12,
+    maxCols: 12,
+    allowMultiLayer: true,
+    defaultLayerIndex: 1,
+    maxLayerIndex: 2,
+    baseLayerIndex: 1,
+  };
+
+  dashboard!:Array<GridsterItem>;
+
+  constructor() {
+    this.dashboard = [
+      {cols: 2, rows: 1, y: 0, x: 0},
+      {cols: 2, rows: 2, y: 0, x: 2}
+    ];
+  }
 
   ngOnInit(): void {
   }
